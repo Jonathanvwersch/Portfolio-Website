a6353613b1035db3b155dd748aa548a7108233d8
Jonathan van Wersch <jonathanvwersch@gmail.com>
2022-04-10 17:15:11 +0100

    Simple redirect

--- src/components/BlogPostCard/BlogPostCard.tsx
+++ src/components/BlogPostCard/BlogPostCard.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@rebass/grid"
+import { navigate } from "gatsby"
 import * as React from "react"
 import { useContext, useRef } from "react"
 import styled, { ThemeContext } from "styled-components"
@@ -29,27 +30,30 @@ const BlogPostCard = ({
   const isVisible = useVisibleOnScreen(domRef, true)
 
   return (
-    <PostLink as="a" ref={domRef} href={`/blog/${path}`} isVisible={isVisible}>
-      <Post>
-        <Thumbnail src={img} alt={title} isOnFrontPage={isOnFrontPage} />
-        <Box p={theme.spacers.size20}>
-          <header>
-            <Box mb={theme.spacers.size8}>
-              <H2 styledAs="h4">{title}</H2>
-            </Box>
-            <Paragraph margin={`0px 0px ${theme.spacers.size16} 0px`}>
-              {date}
-            </Paragraph>
-          </header>
-          <Paragraph
-            fontColor={theme.colors.greyText}
-            fontSize={theme.typography.fontSizes.size14}
-          >
-            {excerpt}
+    <Post
+      as="article"
+      onClick={() => navigate(`/blog/${path}`)}
+      isVisible={isVisible}
+      ref={domRef}
+    >
+      <Thumbnail src={img} alt={title} isOnFrontPage={isOnFrontPage} />
+      <Box p={theme.spacers.size20}>
+        <header>
+          <Box mb={theme.spacers.size8}>
+            <H2 styledAs="h4">{title}</H2>
+          </Box>
+          <Paragraph margin={`0px 0px ${theme.spacers.size16} 0px`}>
+            {date}
           </Paragraph>
-        </Box>
-      </Post>
-    </PostLink>
+        </header>
+        <Paragraph
+          fontColor={theme.colors.greyText}
+          fontSize={theme.typography.fontSizes.size14}
+        >
+          {excerpt}
+        </Paragraph>
+      </Box>
+    </Post>
   )
 }
 
@@ -60,20 +64,17 @@ const Thumbnail = styled.img<{ isOnFrontPage?: boolean }>`
   object-position: center center;
 `
 
-const PostLink = styled(FadeInAndTranslateSection)`
+const Post = styled(FadeInAndTranslateSection)`
   flex-grow: 1;
   min-width: 300px;
   flex-basis: 0;
   min-height: 150px;
-`
-
-const Post = styled.article`
-  height: 100%;
   box-shadow: ${({ theme }) => theme.colors.backgrounds.lightbox};
   overflow: hidden;
   border-radius: ${({ theme }) => theme.sizes.borderRadius[SIZES.LARGE]};
   cursor: pointer;
   background-color: ${({ theme }) => theme.colors.backgrounds.lightbox};
+  border: solid 1px ${({ theme }) => theme.colors.primary};
 
   &:hover {
     transform: scale(1.02);
