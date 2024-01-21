a1f93dbbaea002acf234c98e9c37eb57c1374ec6
jonathanvwersch <jonathanvwersch@gmail.com>
2024-01-21 14:00:20 +0000

    use yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@jvw`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
+  
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
